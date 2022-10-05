b05ffff1ce7fa5814ed78e7c526dc8c8a53dcae7
TrulsHafnor <Truls.Hafnor@outlook.com>
2022-10-05 10:57:52 +0200

    Added game controller, repository, service nad fixed hibernate. Also added an exeption. Nxt is to fix DTOs and setup swagger

--- src/main/java/academy/noroff/hvz/models/Game.java
+++ src/main/java/academy/noroff/hvz/models/Game.java
@@ -1,6 +1,8 @@
 package academy.noroff.hvz.models;
 
+import com.sun.istack.NotNull;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -12,7 +14,14 @@ public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "game_id")
-    private Long id;
+    private int id;
+    @NotNull
+    @Column(length = 50, nullable = false)
+    private String gameTitle;
+    private float nw_lat;
+    private float nw_lng;
+    private float se_lat;
+    private float se_lng;
 
 
 }
