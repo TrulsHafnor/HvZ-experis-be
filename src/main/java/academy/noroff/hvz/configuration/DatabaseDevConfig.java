0467cdd94e80eb328da0e73255d69dc24c6670bf
TrulsHafnor <Truls.Hafnor@outlook.com>
2022-10-06 15:16:28 +0200

    Satt opp flere brukere pluss fikset på application.properties filen

---
+++ src/main/java/academy/noroff/hvz/configuration/DatabaseDevConfig.java
@@ -0,0 +1,22 @@
+package academy.noroff.hvz.configuration;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("dev")
+public class DatabaseDevConfig {
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/HvZ");
+        dataSourceBuilder.username("postgres");
+        dataSourceBuilder.password("postgres");
+        return dataSourceBuilder.build();
+    }
+}
